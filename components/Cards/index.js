b85d0f295f1a226b3760a1ead780936a05c5a453
Sydney Blom <sydneyblom@#gmail.com>
2019-09-06 12:55:18 -0500

    final product

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,66 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+
+    if (response.data) {
+//turn object into array
+        let articles = Object.values(response.data.articles);
+
+        articles.forEach(function (topic) {
+            topic.forEach(function (article) {
+                createCard(article);
+
+            });
+        });
+    }
+ })
+  .catch((error) => {
+console.log(error);
+});
+
+
+
+  function createCard (article) {
+    const cards = document.querySelector('.cards-container');
+
+    //   console.log(article);
+    const card = document.createElement ('div')
+    const headline = document.createElement ('div')
+    const author = document.createElement ('div')
+    const cardImg = document.createElement ('div')
+    const cardImgurl = document.createElement ('img')
+    const authorName = document.createElement ('span')
+
+    
+    
+    card.classList.add('card');
+    author.classList.add('author');
+    headline.classList.add('headline');
+    cardImg.classList.add('img-container');
+    
+    
+    cardImgurl.src = article.authorPhoto;;
+    headline.textContent = article.headline;    
+    authorName.textContent = `By: ${article.authorName}`;
+
+
+    cardImg.appendChild(cardImgurl);
+
+
+    author.append(cardImg);
+
+    author.append(authorName);
+
+    card.append(headline);
+
+    card.append(author);
+ 
+    cards.appendChild(card);
+
+    // console.log(card);
+    }      
+   
